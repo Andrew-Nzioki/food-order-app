e6192f676c92bde3c00c79e070fd14a4273c2598
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-05-14 06:36:46 +0300

    food order app context

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,18 +1,95 @@
-import React from "react";
-import classes from "./MealItemForm.module.css";
-import Input from "../../UI/Input";
-const MealItemForm = () => {
+// import React, { useRef, useState } from "react";
+// import classes from "./MealItemForm.module.css";
+// import Input from "../../UI/Input";
+// const MealItemForm = (props) => {
+//   const [amountIsValid, setAmountIsValid] = useState(true);
+//   const amountInputRef = useRef();
+
+//   //handle the submit data event of the form
+//   const submitHandler = (event) => {
+//     event.preventDefault();
+   
+//     const enteredAmount = amountInputRef.current.value;
+//     console.log(amountInputRef.current.value)
+//     const enteredAmountNumber = +enteredAmount;
+//     if (
+//       enteredAmount.trim().length === 0 ||
+//       enteredAmountNumber < 1 ||
+//       enteredAmountNumber > 5
+//     ) {
+//       setAmountIsValid(false);
+//       return;
+//     }
+//     props.onAddToCart(enteredAmountNumber);
+//   };
+
+//   //
+//   return (
+//     <form className={classes.form} onSubmit={submitHandler}>
+//       <Input
+//         ref={amountInputRef}
+//         label="Amount"
+//         input={{
+//           id: "amount",
+//           type: "number",
+//           min: "1",
+//           max: "5",
+//           step: "1",
+//           defaultValue: "1",
+//         }}
+//       />
+//       <button type="submit">+Add</button>
+//       {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
+//     </form>
+//   );
+// };
+
+// export default MealItemForm;
+
+
+import { useRef, useState } from 'react';
+
+import Input from '../../UI/Input';
+import classes from './MealItemForm.module.css';
+
+const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
-      <Input label="Amount" input={{
-        id:"amount",
-        type:"number",
-        min:"1",
-        max:"5",
-        step:"1",
-        defaultValue:"1"
-      }}/>
-      <button>+Add</button>
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        ref={amountInputRef}
+        label='Amount'
+        input={{
+          id: 'amount_' + props.id,
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }}
+      />
+      <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,10 +1,44 @@
-import React from "react";
+// import React from "react";
 
-import ReactDOM from "react-dom";
-import classes from "./Modal.module.css";
+// import ReactDOM from "react-dom";
+// import classes from "./Modal.module.css";
+// const Backdrop = (props) => {
+//   return <div className={classes.backdrop} onClick={props.onClose}></div>;
+// };
+// const ModalOverlay = (props) => {
+//   return (
+//     <div className={classes.modal}>
+//       <div className={classes.content}>{props.children}</div>
+//     </div>
+//   );
+// };
+
+// const portalElement = document.getElementById("overlays");
+// const Modal = (props) => {
+//   return (
+//     <>
+//       {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+
+//       {ReactDOM.createPortal(
+//         <ModalOverlay>{props.children}</ModalOverlay>,
+//         portalElement
+//       )}
+//     </>
+//   );
+// };
+
+// export default Modal;
+// // 
+
+import { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+import classes from './Modal.module.css';
+
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} onClick={props.onClose}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
+
 const ModalOverlay = (props) => {
   return (
     <div className={classes.modal}>
@@ -13,17 +47,17 @@ const ModalOverlay = (props) => {
   );
 };
 
-const portalElement = document.getElementById("overlays");
+const portalElement = document.getElementById('overlays');
+
 const Modal = (props) => {
   return (
-    <>
+    <Fragment>
       {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
-
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
       )}
-    </>
+    </Fragment>
   );
 };
 
